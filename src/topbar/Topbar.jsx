7afe11f87e7220dbbb0d24b96b083d26e1d3ef67
Sushant <sushant.adhikari.619@gmail.com>
2021-06-05 18:57:52 +0545

    navbar

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import './topbar.css'
+
+const Topbar = () => {
+    return (
+        <div className = "top">
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+                <i className="topIcon fab fa-pinterest-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img
+                    className="topImg"
+                    src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+                    alt="profile picture"
+                />
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
