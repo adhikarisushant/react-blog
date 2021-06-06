1b43bdbdd597b47e7cebac210a6d57c00a7b8e3b
Sushant <sushant.adhikari.619@gmail.com>
2021-06-06 20:10:18 +0545

    homepage posts

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -5,6 +5,11 @@ export default function Posts() {
     return (
         <div className="posts">
             <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
