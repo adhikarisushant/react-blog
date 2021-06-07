caf5898722beace91d5b098595ab5e74a9a28e2f
Sushant <sushant.adhikari.619@gmail.com>
2021-06-07 13:01:27 +0545

    settings page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <>
       <Topbar />
-      <Write />
+      <Settings />
       </>
     </div>
   );
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,33 @@
+import "./settings.css"
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function Settings() {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteTitle">Delete Account</span>
+                </div>
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="profile picture" />
+                        <label htmlFor="fileInput">
+                            <i className="settingsPPIcon  far fa-user-circle"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display: "none"}} />
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder="Sushant" />
+                    <label>Email</label>
+                    <input type="email" placeholder="sushant@email.com" />
+                    <label>Password</label>
+                    <input type="password" />
+                    <button className="settingsSubmit">Update</button>
+                </form>
+            </div>
+            <Sidebar />
+        </div>
+    )
+}
