404b3d4c1ad1be0dca2fd5b97e7be9bc58df32ce
Sushant <sushant.adhikari.619@gmail.com>
2021-06-07 13:33:18 +0545

    register and login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
     <div className="App">
       <>
       <Topbar />
-      <Settings />
+      <Register />
       </>
     </div>
   );
