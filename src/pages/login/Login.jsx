9bb60117dfb0c1414793528fb6fda600d7e7b18a
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-06-29 18:13:09 +0545

    context api setup

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,29 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom"
 import "./login.css"
 
 export default function Login() {
+
+    const userRef = useRef();
+    const passwordRef = useRef();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input type="text" className="loginInput" placeholder="Enter your Email..." />
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input type="text" className="loginInput" placeholder="Enter your Username..."
+                ref={userRef}
+                />
                 <label>Password</label>
-                <input type="password" className="loginInput" placeholder="Enter your Password..." />
-                <button className="loginButton">Login</button>
+                <input type="password" className="loginInput" placeholder="Enter your Password..."
+                ref={passwordRef}
+                />
+                <button className="loginButton" type="submit">Login</button>
             </form>
                 <button className="loginRegisterButton">
                     <Link className="link" to="/register">Register</Link>
