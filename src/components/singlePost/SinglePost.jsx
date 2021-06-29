8f4cc846adbefa0c328373f66816385af774fae9
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-06-29 12:25:13 +0545

    using query in axios

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation, Link } from "react-router-dom"
 import "./singlePost.css"
 
 export default function SinglePost() {
@@ -32,7 +32,11 @@ export default function SinglePost() {
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+                    <span className="singlePostAuthor">Author:
+                        <Link to={`/?user=${post.username}`} className="link">
+                            <b>{post.username}</b>
+                        </Link>
+                        </span>
                     <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
@@ -7,14 +8,15 @@ import "./home.css";
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
+    const {search} =useLocation();
 
     useEffect(() =>{
         const fetchPosts = async()=>{
-            const res = await axios.get("/posts")
+            const res = await axios.get("/posts"+search)
             setPosts(res.data)
         }
         fetchPosts()
-    }, [])
+    }, [search])
 
     return (
         <>
