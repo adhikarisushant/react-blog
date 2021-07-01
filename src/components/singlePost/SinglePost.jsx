a7f4cba5534870f1ec60f214b7c34b8b73cc7b0e
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-07-01 10:45:25 +0545

    delete post and edit post

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,21 +1,49 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom"
+import { Context } from "../../context/Context";
 import "./singlePost.css"
 
 export default function SinglePost() {
     const location = useLocation()
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({})
+    const {user} = useContext(Context)
     const PF = "http://localhost:5000/images/";
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
     useEffect(() => {
         const getPost = async () => {
             const res = await axios.get("/posts/" + path);
-            setPost(res.data)
+            setPost(res.data);
+            setTitle(res.data.title);
+            setDesc(res.data.desc);
         };
         getPost()
     }, [path]);
+
+    const handleDelete = async() => {
+        try{
+            await axios.delete(`/posts/${post._id}`, {data: {username:user.username},});
+            window.location.replace("/");
+        } catch(err){
+
+        }  
+    };
+
+    const handleUpdate = async () => {
+        try{
+            await axios.put(`/posts/${post._id}`, {
+                username:user.username, title, desc, 
+            });
+            setUpdateMode(false)
+        } catch(err){
+
+        }  
+    }
+
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
@@ -24,14 +52,23 @@ export default function SinglePost() {
                         className="singlePostImg"
                         src={PF + post.photo} alt="" />
                 )}
+                {
+                    updateMode ? <input type="text" value={title} className="singlePostTitleInput"
+                    autoFocus
+                    onChange={(e) => setTitle(e.target.value)} />: (
+                        <h1 className="singlePostTitle">
+                        {title}
+                        {post.username === user?.username && (
+                            <div className="singlePostEdit">
+                            <i className="singlePostIcon far fa-edit" onClick={() => setUpdateMode(true)}></i>
+                            <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+                            </div>
+                        )}
+    
+                    </h1>
+                    )
+                }
 
-                <h1 className="singlePostTitle">
-                    {post.title}
-                <div className="singlePostEdit">
-                    <i className="singlePostIcon far fa-edit"></i>
-                    <i className="singlePostIcon far fa-trash-alt"></i>
-                    </div>
-                </h1>
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">Author:
                         <Link to={`/?user=${post.username}`} className="link">
@@ -40,9 +77,15 @@ export default function SinglePost() {
                         </span>
                     <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDesc">
-                    {post.desc}
-                </p>
+                {updateMode ? <textarea className="singlePostDescInput" value={desc} onChange={(e) => setDesc(e.target.value)} /> : (
+                    <p className="singlePostDesc">
+                    {desc}
+                    </p>
+                )}
+                {updateMode && (
+                <button className="singlePostButton" onClick={handleUpdate}>Update</button>
+                )}
+
             </div>
         </div>
     )
